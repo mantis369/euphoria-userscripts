6b90579308cd1e843048bf5b0faf5d9c15e387a7
mantis <me@derikstiller.com>
2015-01-24 02:58:17 -0500

    better highlights and unread counter
    - make highlight green and compatible with timestamp text
    - 60 second highlight window (down from 5 mins)
    - new downthread count and total unread count

--- highlight-new.js
+++ highlight-new.js
@@ -1,8 +1,6 @@
 var css = [
-            "div.messages-container { padding-left:7px!important; }",
-            "time { left:-20px!important; width:5px!important; }",
-            "time.highlight { background:red!important; }",
-            "div.topcount { border:1px #CCC solid; color:red; position:absolute; right:50px; text-align:center; top:10px; width:40px; z-index:100; }"
+            "time.highlight { border-right:5px #0F0 solid!important; box-sizing:border-box; }",
+            "div.counter-display { background:#0F0; border:2px green solid; color:black; font-family:monospace; font-size:14px; padding:0 2px 0 0; position:absolute; right:150px; text-align:center; top:36px; width:60px; z-index:100; }"
           ].join("\n");
 var head = document.head || document.getElementsByTagName("head")[0];
 var style = document.createElement("style");
@@ -20,9 +18,9 @@ Heim.actions.sendMessage.listen(function() {
     last_t = new Date();
 }); 
 
-var topcount = document.createElement("div");
-    topcount.className = "topcount";
-document.body.appendChild(topcount);    
+var counter_div = document.createElement("div");
+    counter_div.className = "counter-display";
+document.body.appendChild(counter_div);    
 
 function computeOffsetTop(element) {
     var offset = 0;
@@ -39,23 +37,30 @@ function annotate(no_highlight) {
     var i, time_t, delta,
         is_new = false,
         scroll_top = container.scrollTop,
-        offscreen_count = 0,
+        screen_height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+        offset,
+        upcount = 0,
+        downcount = 0,
+        unread_count = 0,
         times = document.querySelectorAll("time");
     for (i = 0; i < times.length; i++) {
         time_t = new Date(times[i].getAttribute("datetime"));
         delta = last_t - time_t;
-        is_new = (delta < 300000);
+        is_new = (delta < 60000);
         if (!no_highlight) {
             times[i].className = (is_new) ? "highlight" : "";
-            times[i].innerHTML = "";
-            times[i].parentNode.querySelector("span.nick")
-                    .setAttribute("title", time_t.toLocaleString());
         }
-        if (is_new && (computeOffsetTop(times[i]) < scroll_top)) {
-            offscreen_count++;
+        if (is_new) {
+            offset = computeOffsetTop(times[i]);
+            unread_count++;
+            if (offset < scroll_top) {
+                upcount++;
+            } else if (offset > (scroll_top + screen_height)) {
+                downcount++;
+            }
         }
     }
-    topcount.innerHTML = "^ " + offscreen_count + " ^";
+    counter_div.innerHTML = [upcount, unread_count, downcount].join("|"); 
 }
 
 var observer = new window.MutationObserver(function(mutations) {
